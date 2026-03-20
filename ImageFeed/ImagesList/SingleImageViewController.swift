3f6582ca69463ab1affdcc2147b0e831b4cd5a53
Practicum <shepelevart1995@gmail.com>
2026-03-20 14:03:40 +0300

    Добавлены исправления по рекомендациям ревьюера

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -62,28 +62,20 @@ final class SingleImageViewController: UIViewController {
     
     // MARK: - Image Scaling
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
         view.layoutIfNeeded()
-        
-        let scrollViewSize = scrollView.bounds.size
+        let visibleRectSize = scrollView.bounds.size
         let imageSize = image.size
-        
-        guard imageSize.width > 0, imageSize.height > 0 else { return }
-        
-        let scale = scrollViewSize.width / imageSize.width
-        
-        let scaledWidth = imageSize.width * scale
-        let scaledHeight = imageSize.height * scale
-        
-        imageView.frame = CGRect(
-            origin: .zero,
-            size: CGSize(width: scaledWidth, height: scaledHeight)
-        )
-                
-        scrollView.minimumZoomScale = scale
-        scrollView.maximumZoomScale = 1.25
-        scrollView.zoomScale = scale
-        
-        centerImage()
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
     
     private func centerImage() {
